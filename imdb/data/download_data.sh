06168920d2689cc2a822bf8729fb78c1661d3f25
LRojon <luisrojon@lasquinceletras.com.mx>
2019-12-07 17:05:04 -0600

    Corrección en el código para cargar los archivos tsv a postgres, y adicion del codigo .sh para descargar los datos en linea.

---
+++ imdb/data/download_data.sh
@@ -0,0 +1,9 @@
+wget -i data_imdb.txt
+gunzip title.ratings.tsv.gz 
+gunzip name.basics.tsv.gz
+gunzip title.basics.tsv.gz
+gunzip title.episode.tsv.gz
+gunzip title.akas.tsv.gz
+gunzip title.crew.tsv.gz
+gunzip title.principals.tsv.gz
+
