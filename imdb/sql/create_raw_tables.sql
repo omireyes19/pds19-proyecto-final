ad69e09be1ed3d94782be8e4e309cfc559b303aa
omireyes19 <vagrant@ubuntu1904.localdomain>
2019-12-11 19:13:35 +0000

    Fix bug in create raw tables file

--- imdb/sql/create_raw_tables.sql
+++ imdb/sql/create_raw_tables.sql
@@ -3,12 +3,12 @@ create schema if not exists raw;
 drop table if exists raw.name_basics;
 
 create table raw.name_basics (
-	"nconst" TEXT,
-	"primaryName" TEXT,
-	"birthYear" TEXT,
-	"deathYear" TEXT,
-	"primaryProfession" TEXT,
-	"knownForTitles" TEXT
+  "nconst" TEXT,
+  "primaryName" TEXT,
+  "birthYear" TEXT,
+  "deathYear" TEXT,
+  "primaryProfession" TEXT,
+  "knownForTitles" TEXT
 
 );
 
@@ -17,15 +17,15 @@ comment on table raw.name_basics is 'describe las características estáticas de
 drop table if exists raw.title_basics;
 
 create table raw.title_basics (
-	"tconst" TEXT,
-  	"titleType" TEXT,
-  	"primaryTitle" TEXT,
-  	"originalTitle" TEXT,
-  	"isAdult" TEXT,
-  	"startYear" TEXT,
-  	"endYear" TEXT,
-  	"runtimeMinutes" TEXT,
-  	"genres" TEXT
+  "tconst" TEXT,
+    "titleType" TEXT,
+    "primaryTitle" TEXT,
+    "originalTitle" TEXT,
+    "isAdult" TEXT,
+    "startYear" TEXT,
+    "endYear" TEXT,
+    "runtimeMinutes" TEXT,
+    "genres" TEXT
 );
 
 comment on table raw.title_basics is 'describe las características de las obras con su formato audiencia, anos, generos duracion, etc.';
@@ -33,10 +33,10 @@ comment on table raw.title_basics is 'describe las características de las obras
 drop table if exists raw.title_episode;
 
 create table raw.title_episode (
-	"tconst" TEXT,
-  	"parentTconst" TEXT,
-  	"seasonNumber" TEXT,
-  	"episodeNumber" TEXT
+  "tconst" TEXT,
+    "parentTconst" TEXT,
+    "seasonNumber" TEXT,
+    "episodeNumber" TEXT
 );
 
 comment on table raw.title_episode is 'describe las características de los episodios de las series';
@@ -44,35 +44,51 @@ comment on table raw.title_episode is 'describe las características de los epis
 drop table if exists raw.title_ratings;
 
 create table raw.title_ratings (
-	"tconst" TEXT,
-  	"averageRating" TEXT,
-  	"numVotes" TEXT
+  "tconst" TEXT,
+    "averageRating" TEXT,
+    "numVotes" TEXT
 );
 
 comment on table raw.title_ratings is 'describe las características de los ratings de las obras';
 
-drop table if exists raw.title_akas;
+drop table if exists raw.title_akas1;
 
-create table raw.title_akas (
-	"titleId" TEXT,
-  	"ordering" TEXT,
-  	"title" TEXT,
-	"region" TEXT,
-  	"language" TEXT,
-  	"types" TEXT,
-  	"attributes" TEXT,
-  	"isOriginalTitle" TEXT
+create table raw.title_akas1 (
+  "titleId" TEXT,
+    "ordering" TEXT,
+    "title" TEXT,
+  "region" TEXT,
+    "language" TEXT,
+    "types" TEXT,
+    "attributes" TEXT,
+    "isOriginalTitle" TEXT
 
 );
 
-comment on table raw.title_akas is 'describe las características de los titulos en distintas regiones y lenguas';
+comment on table raw.title_akas1 is 'describe las características de los titulos en distintas regiones y lenguas parte 1';
+
+drop table if exists raw.title_akas2;
+
+create table raw.title_akas2 (
+  "titleId" TEXT,
+    "ordering" TEXT,
+    "title" TEXT,
+  "region" TEXT,
+    "language" TEXT,
+    "types" TEXT,
+    "attributes" TEXT,
+    "isOriginalTitle" TEXT
+
+);
+
+comment on table raw.title_akas2 is 'describe las características de los titulos en distintas regiones y lenguas parte 2';
 
 drop table if exists raw.title_crew;
 
 create table raw.title_crew (
-	"tconst" TEXT,
-  	"directors" TEXT,
-  	"writers" TEXT
+  "tconst" TEXT,
+    "directors" TEXT,
+    "writers" TEXT
 );
 
 comment on table raw.title_crew is 'describe las características de los directores y escritores de las obras';
@@ -80,12 +96,64 @@ comment on table raw.title_crew is 'describe las características de los directo
 drop table if exists raw.title_principals;
 
 create table raw.title_principals (
-	"tconst" TEXT,
-  	"ordering" TEXT,
-  	"nconst" TEXT,
-  	"category" TEXT,
-  	"job" TEXT,
-  	"characters" TEXT
+  "tconst" TEXT,
+    "ordering" TEXT,
+    "nconst" TEXT,
+    "category" TEXT,
+    "job" TEXT,
+    "characters" TEXT
 );
 
-comment on table raw.title_crew is 'describe las características de las tareas que realizan las personas en las peliculas';
+comment on table raw.title_principals is 'describe las características de las tareas que realizan las personas en las peliculas parte 1';
+
+drop table if exists raw.title_principals1;
+
+create table raw.title_principals1 (
+  "tconst" TEXT,
+    "ordering" TEXT,
+    "nconst" TEXT,
+    "category" TEXT,
+    "job" TEXT,
+    "characters" TEXT
+);
+
+comment on table raw.title_principals1 is 'describe las características de las tareas que realizan las personas en las peliculas parte 2';
+
+drop table if exists raw.title_principals2;
+
+create table raw.title_principals2 (
+  "tconst" TEXT,
+    "ordering" TEXT,
+    "nconst" TEXT,
+    "category" TEXT,
+    "job" TEXT,
+    "characters" TEXT
+);
+
+comment on table raw.title_principals2 is 'describe las características de las tareas que realizan las personas en las peliculas parte 3';
+
+drop table if exists raw.title_principals3;
+
+create table raw.title_principals3 (
+  "tconst" TEXT,
+    "ordering" TEXT,
+    "nconst" TEXT,
+    "category" TEXT,
+    "job" TEXT,
+    "characters" TEXT
+);
+
+comment on table raw.title_principals3 is 'describe las características de las tareas que realizan las personas en las peliculas parte 4';
+
+drop table if exists raw.title_principals4;
+
+create table raw.title_principals4 (
+  "tconst" TEXT,
+    "ordering" TEXT,
+    "nconst" TEXT,
+    "category" TEXT,
+    "job" TEXT,
+    "characters" TEXT
+);
+
+comment on table raw.title_principals4 is 'describe las características de las tareas que realizan las personas en las peliculas parte 5';
